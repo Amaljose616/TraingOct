93cc6c3e95e3cfd611d4f6529188705693792fc7
amaljose <amal.jose@vonnue.com>
2022-12-12 15:18:18 +0530

    feat:java script

--- js-tutorials/index.js
+++ js-tutorials/index.js
@@ -17,7 +17,7 @@
 //  let dogAge = age * humanDogRatio
 //  console.log (dogAge)
 
-  let number = 1
+//   let number = 1
 
 //  number = number + 1
 //   console.log  (number)
@@ -266,26 +266,26 @@
 // }
 // console.log (rollDice())
 
-let x = 100
-let y = 20
-let z = 50
+// let x = 100
+// let y = 20
+// let z = 50
 
-function greatest() {
-    if (x > y){
-        if (x > z){
-            return x
-        }
-    } else if (y > x){
-        if(y > z){
-            return y
-        }
-    }else{
-        return z
-    }
-}
-let result = greatest()
+// function greatest() {
+//     if (x > y){
+//         if (x > z){
+//             return x
+//         }
+//     } else if (y > x){
+//         if(y > z){
+//             return y
+//         }
+//     }else{
+//         return z
+//     }
+// }
+// let result = greatest()
 
-console.log (result)
+// console.log (result)
 
 // let Name = "Amal"
 // let Age = 21 
@@ -344,21 +344,21 @@ console.log (result)
 //     divEl.innerHTML = "<p>ThankYou </p>"
 // }
 
-let a = 1
-let p = "not a prime number"
-let q = "it's prime number"
-for(let i = 2 ; i < a ; i++){
-    if(a % i === 0){
-        console.log(p)
-    }
-        else{
-        console.log(q)
-    }
-}if(a===2){
-    console.log(q)
-}else if (a == 1){
-    console.log("neither a prime nor a composite")
-}
+// let a = 1
+// let p = "not a prime number"
+// let q = "it's prime number"
+// for(let i = 2 ; i < a ; i++){
+//     if(a % i === 0){
+//         console.log(p)
+//     }
+//         else{
+//         console.log(q)
+//     }
+// }if(a===2){
+//     console.log(q)
+// }else if (a == 1){
+//     console.log("neither a prime nor a composite")
+// }
 
 // let a = 10
 // let x = "not a prime number"
@@ -420,3 +420,209 @@ for(let i = 2 ; i < a ; i++){
 //     console.log("hello " + name)
 // }
 // greet ("amal")
+// var changed = 0 
+// function change (num){
+// return  (num +5)/3
+// }
+// changed =change(10) 
+
+// var processed = 0
+// function processArg (num){
+//     return (num + 3)/5    
+// } 
+// processed = processArg(7)
+
+// function ourTrueOrFalse(isItTrue){
+//     if(isItTrue){
+//         return "yes, it's true"
+//     }
+//     return "no, it's false"
+// }
+// function ourTrueOrFalse(wasThatTrue){
+// if (wasThatTrue){
+//     return "yes,that was true"
+// }
+// return "no ,that was false"
+// }
+// console.log (ourTrueOrFalse(true))
+
+// function testEqual(val){
+//     if (val == 12){
+//         return "equal"
+//     }
+//     return "not equal"
+// }
+// console.log(testEqual(10))
+
+// function orderMyLogic(val){
+//     if (val < 10){
+//         return "Less than 10"
+//     }else if (val < 5){
+//         return "Less than 5"
+//     }else {
+//         return"Greater than or equal to 10"
+//     }
+// }
+// console.log(orderMyLogic(7))
+
+// var names = ["Hole-in-one!","Eagle","Birdie","par","Bogey","Double Bogey","Go-home"]
+
+// function golfScore(par,strokes){
+//     if(strokes == 1){
+//         return names[0]
+//     }else if (strokes <= par -2){
+//         return names[1]
+//     }else if(strokes == par -1){
+//         return names[2]
+//     }else if (strokes == par){
+//         return names[3]
+//     }else if (strokes == par + 1){
+//         return names [4]
+//     }else if (strokes == par +2 ){
+//         return names[5]
+//     }else if (strokes >= par + 3){
+//         return names[6]
+//     }
+// return "Change Me"
+// }
+// console.log (golfScore(5,4)) 
+
+// function caseInSwitch(val){
+//     var answer = ""
+//     switch(val){
+//         case 1:
+//             answer = "alpha"
+//             break 
+//         case 2:
+//             answer = "beta"  
+//             break
+//         case 3:
+//             answer = "gamma"   
+//             break
+//         case 8:
+//             answer = "delta"   
+//             break
+//     }
+//     return answer
+// }
+// console.log(caseInSwitch(8)
+
+// function isLess(a,b){
+//     return a < b
+// }
+// console.log(isLess(5,))
+
+// function abTest(a,b) {
+//     if (a < 0 || b < 0){
+//         return undefined
+//     }
+//     return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2))
+// }
+// console.log(abTest(-2,2))
+
+// var count = 0
+//  function cc(card){
+//     switch (card){
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//         case 6:
+//             count ++
+//             break
+//        case 10:
+//        case "J":
+//        case "Q":
+//        case "K": 
+//        case "A":
+//         count --
+//         break   
+//     }
+//     var holdbet = 'Hold'
+//     if (count > 0){
+//         holdbet = 'Bet'
+//     }
+//     return count + " " + holdbet
+//  }
+//  cc(2); cc('K'); cc(10); cc ('k'); cc('A');
+//  console.log(4)
+
+// var ourDog = {
+//     "name":"Camper",
+//     "legs":4,
+//     "tails":1,
+//     "friends" : ["everything!"]
+// }
+// var myDog = {
+//     "name":"chikku",
+//     "legs":3,
+//     "tails":2,
+//     "friends":[]
+// }
+
+// var testObj = {
+//     "hat": "ballcap",
+//     "shirt":"jersy",
+//     "shoe":"cleats"
+// }
+// var hatValue = testObj.hat
+// var shirtValue = testObj.shirt
+// console.log(hatValue)
+// console.log(shirtValue)
+
+//  var myMusic = [
+//     {
+//         "artist":"Biller joel",
+//         "title": "piano man",
+//         "release_year:1973",
+//         "formates":[
+//             "cd","8t","lp"
+//         ],
+//         "gold":true
+//     },
+//     {
+//         "artist":"Beau Carnnes",
+//         "title":"Cereal Man",
+//         "release_year":2003,
+//         "formates":[
+//             "youtube video"
+//         ]
+//     }
+//  ]
+
+// var myStorage = {
+//     "car": {
+//         "inside" :{
+//             "glove box":"maps",
+//             "passenger seat":"crumbs"
+//         },
+    
+//     "outside":{
+//         "trunk":"jack"
+//     }
+// }
+// }
+// var gloveBoxContents = myStorage.car.inside["glove box"]
+// console.log(gloveBoxContents)
+
+var myPlants = [
+    {
+        type:"flowers",
+        list:[
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type:"trees",
+        list : [
+            "fir",
+            "pine",
+            "birch"
+        ]
+
+    }
+]
+var secondTree = myPlants[1].list[1]
+console.log(secondTree)
