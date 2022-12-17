68b7369880a77598357cc1f153ab7bb670846f99
amaljose <amal.jose@vonnue.com>
2022-12-17 13:45:23 +0530

    js tutorials

--- js-tutorials/index.js
+++ js-tutorials/index.js
@@ -1208,35 +1208,83 @@
 // document.querySelector('.score').textContent = 20;
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+// let highScore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
-  //   console.log(document.querySelector('.guess').value);
-  //   document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+//   document.querySelector('.message').textContent = 'Correct Number';
 
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'No number!';
+//   } else if (guess === secretNumber) {
+//     document.querySelector('.message').textContent = 'Correct Number!';
+//     document.querySelector('.number').textContent = secretNumber;
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lose the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
-    score--;
-    document.querySelector('.score').textContent = 0;
-  }
+//     document.querySelector('body').style.backgroundColor = '#60b347';
+//     document.querySelector('.number').style.width = '30rem';
+
+//     if (score > highScore) {
+//       highScore = score;
+//       document.querySelector('.highscore').textContent = highScore;
+//     }
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = 'Too high';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You lose the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   } else if (guess < secretNumber) {
+//     document.querySelector('.message').textContent = 'Too Low';
+//     score--;
+//     document.querySelector('.score').textContent = 0;
+//   }
+// });
+
+// document.querySelector('.again').addEventListener('click', function () {
+//   (score = 20), (secretNumber = Math.trunc(Math.random() * 20) + 1);
+//   document.querySelector('.message').textContent = 'Start Guessing...';
+//   document.querySelector('.score').textContent = score;
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('.guess').value = '';
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.number').style.width = '15rem';
+// });
+
+// let n = 91;
+
+// if ((n - 1) % 3 === 0) {
+//   console.log('number is in the form of 3n+1');
+// } else {
+//   console.log('number is not in the form of 3n+1');
+//
+
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 });
