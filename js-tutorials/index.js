6d4a5f346f0b87e92c13eefa19ec14d2a83f67ec
amaljose <amal.jose@vonnue.com>
2022-12-18 17:58:59 +0530

    feat:js tutorials

--- js-tutorials/index.js
+++ js-tutorials/index.js
@@ -1268,34 +1268,71 @@
 
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     console.log('Button clicked');
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
 
-  const closeModal = function(){
-    modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-  }
-  btnCloseModal.addEventListener('click', closeModal) 
-  overlay.addEventListener('click', closeModal)
+//   const closeModal = function(){
+//     modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   }
+//   btnCloseModal.addEventListener('click', closeModal)
+//   overlay.addEventListener('click', closeModal)
 
-  document.addEventListener('keydown', function(){
-    console.log("A key was pressed")
-  })
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-// });
-// overlay.addEventListener('click', function () {
+//   document.addEventListener('keydown', function(){
+//     console.log("A key was pressed")
+//   })
+// btnCloseModal.addEventListener('click', function () {
 //   modal.classList.add('hidden');
 //   overlay.classList.add('hidden');
 // });
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');});
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListner('click', openModal);
+
+// btnCloseModal.addEventListner('click', closeModal);
+// overlay.addEventListner('click', closeModal);
+
+// document.addEventListener('keydown', function () {
+//   console.log('A key was pressed');
+// });
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
