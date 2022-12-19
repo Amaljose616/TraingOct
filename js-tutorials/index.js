5da2ce79f3f87295d6f9710b184bba4bc4ad25ff
amaljose <amal.jose@vonnue.com>
2022-12-19 19:45:40 +0530

    feat:js

--- js-tutorials/index.js
+++ js-tutorials/index.js
@@ -1620,14 +1620,55 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   startMenu: ['Focaccia', 'Bruscchetta', 'Garlic Bread', 'Caprese salad'],
   mainmenu: ['pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [first, second] = restaurant.categories;
-console.log(first, second);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// const temp = main;
+// main = secondary
+// secondary = temp
+// console.log(main, secondary)
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]]
+// const [i, ,j] = nested;
+// console.log(i,j)
+
+// const[i, [j,k]] = nested
+// console.log(i,j,k)
+
+// const [p, q, r] = [8,9]
+// console.log(p, q, r)
