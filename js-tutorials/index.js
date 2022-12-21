a4cd7e4930882706e9e03d39025a30fbd00838df
amaljose <amal.jose@vonnue.com>
2022-12-21 19:56:01 +0530

    feat:js

--- js-tutorials/index.js
+++ js-tutorials/index.js
@@ -2263,7 +2263,7 @@
 // const output = `${type.replaceAll('_','')} ${from} ${to} (${time})`
 // console.log(output)
 
-'use strict';
+// 'use strict';
 
 // const bookings = [];
 
@@ -2357,31 +2357,339 @@
 
 // greetArr('hi')('jonas')
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
 
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+// lufthansa.book(239, 'Amal Jose');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   name: 'Eurowings',
+//   iataCode: 'EW',
+//   booking: [],
+// };
+
+// const book = lufthansa.book;
+
+// book.call(eurowings, 23, 'aachi');
+// console.log(eurowings);
+
+// book.call(lufthansa, 239, 'amal');
+// console.log(lufthansa);
+
+// const swiss ={
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+
+// }
+
+// book.call(swiss,583,'mary')
+// connsole.log(swiss)
+
+// const flightData = [583, 'amaljose']
+// book.appply[swiss,flightData]
+// console.log(swiss)
+
+// book.call(swiss, ...flightData)
+
+// const bookW = book.bind(eurowings)
+// const bookLH = book.bind(lufthansa)
+// const bookLX = book.bind(swiss)
+// bookEW(23,'steven')
+
+// const bookEW23 = book.bind(eurowings, 123)
+// bookEW23('martha')
+
+// lufthansa.planes = 300
+// lufthansa.buyPlane = function(){
+//   console.log(this)
+
+//   this.planes++
+//   console.log(this.planes)
+// }
+// lufthansa.buyPlane()
+
+// document.querySelector('.buy').addEventListener(
+//   'click', lufthansa.buyPlane)
+
+// const addTax = (rate, value) => value + value * rate
+// console.log(addtax(0.1,200))
+
+// const addVAT = addtax.bind(null, 0.23)
+
+// console.log(addVAT(100))
+// console.log(addVAT(23))
+
+// const addTaxRate = function(rate){
+//   return function(value){
+//     return value + value * rate
+//   }
+// }
+// const addVAT2 = addTaxRate(0.23)
+
+// const poll = {
+//   question: 'What is your favorite programming language ?',
+//   options: ['0:JavaScript', '1:Python', '2: Rust', '3: C++'],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+//     console.log(this.answers);
+//     this.
+//   },
+//   displayResults(type ='array'){
+//     if(type === 'array'){
+//       console.log(this.answers)
+//     }else if(type === 'string'){
+//       console.log(`Poll results are ${this.answers.join(',')}`)
+//     }
+//   }
+// };
+// // poll.registerNewAnswer();
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//   poll.displayResults.call({answers: [5, 2, 3]},'string')
+//   poll.displayResults.call({answers: [1, 5, 2, 3, 9, 6]},'string')
+//   poll.displayResults.call({answers: [1, 5, 2, 3, 9, 6]})
+
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce()(function () {
+//   console.log('This will never run again');
+//   const isPrivate =23
+// })();
+
+// console.log(isPrivate)
+
+// (() => console.log('This will ALSO never run again'))();
+
+// {
+//   const isPrivate = 23
+//   var notPrivate = 46
+// }
+// console.log(notPrivate)
+
+// const secureBooking = function(){
+//   let passengerCount = 0
+
+//   return function(){
+//     passengerCount++
+//     console.log(`$(passengerCount) passengers`)
+//   }
+// }
+// const booker = secureBooking()
+
+// // ------SLACK-------
+
+// booker()
+// booker()
+// booker()
+
+// console.dir(booker)
+
+// let f;
+// const g = function(){
+//   const a= 23
+//   f=function(){
+//          console.log(a*2)
+//   }
+// }
+// const h = function(){
+//   const b = 777
+//   f=function(){
+//     console.log(b*2)
+//   }
+// }
+// g();
+// f()
+// console.dir(f)
+// h()
+// f();
+
+// console.dir(f)
+
+// const boardPassengers = function(n, wait){
+//   const perGroup = n / 3
+//   setTimeout(function(){},1000)
+//   console.log(`will start boarding in ${wait} seconds`)
+// }
+// setTimeout(function(){
+//   console.log('TIMER')
+// },1000)
+
+// const perGroup = 1000;
+// boardPassengers(180,3)
+
+// (function () {
+//   const header = documents.querrySelector('h1');
+//   header.style.color = 'red';
+
+//   document.querySelector('body').addEventListener
+//   ('click',function(){
+//     header.style.color = 'blue'
+//   })
+// })();
+
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
-lufthansa.book(239, 'Amal Jose');
-console.log(lufthansa);
 
-const eurowings = {
-  name: 'Eurowings',
-  iataCode: 'EW',
-  booking: [],
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
-const book = lufthansa.book;
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
 
-book.call(eurowings, 23, 'aachi');
-console.log(eurowings);
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
 
-book.call(lufthansa, 239, 'amal');
-console.log(lufthansa);
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log([...arr]);
+
+// // console.log(arr.splice(2));
+// arr.splice(-1)
+// console.log(arr)
+// arr.splice(1,2)
+// console.log(arr);
+
+// arr =['a','b','c','d','e']
+// const arr2 = ['j','i','h','g','j']
+// console.log(arr2.reverse())
+// console.log(arr2)
+
+// const letters = arr.concat(arr2)
+// console.log(letters)
+// console.log([...arr, ...arr2])
+
+// console.log(letters.join)
+
+// const arr = [23, 11, 64]
+// console.log(arr[0])
+// console.log(arr.at(0))
+
+// console.log(arr[arr.length - 1])
+// console.log(arr.slice(-1)[0])
+// console.log(arr.at(-1))
+
+// console.log('jonas'.at(0))
+// console.log('jonas'.at(-1))
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log('----FOREACH----');
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, Map) {
+  console.log(`${key}:${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${value}:${value}`);
+});
