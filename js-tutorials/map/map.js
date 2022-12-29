0330d18ac4e7f80607bf866d07d58a2790ba8077
amaljose <amal.jose@vonnue.com>
2022-12-29 20:47:09 +0530

    feat: javascript

---
+++ js-tutorials/map/map.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// prettier -ignnore
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form_input--type');
+const inputDistance = document.querySelector('form_input--duration');
+const inputCadence = document.querySelector('.form_input--cadence');
+const inputElevation = document.querySelector('.form_input--elevation');
