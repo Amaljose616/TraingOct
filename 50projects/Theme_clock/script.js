1f9753e6f3a575014f571c6258a87321836e5b58
Amal jose <amal.jose@vonnue.com>
2023-02-17 22:13:57 +0530

    feat:motherpage_modified

--- 50projects/Theme_clock/script.js
+++ 50projects/Theme_clock/script.js
@@ -40,6 +40,7 @@ function setTime() {
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    
 }
 
 setTime()
