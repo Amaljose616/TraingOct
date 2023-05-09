54652b3e25d78af1069cb8cf65b1c6c3ea5dd0b4
Amal jose <amal.jose@vonnue.com>
2023-05-09 13:18:23 +0530

    Bash script

--- Bash/new.sh
+++ Bash/new.sh
@@ -1,13 +1,53 @@
 #! /usr/bin/bash
-read -p "enter name:" name
-read -sp "password:" pass
-echo 
-if [ $pass -eq 1234 ]
-then
-echo "WELCOME $name"
-else
-echo "wrong password"
-fi
+
+# echo -e "enter age: \c"
+# read age
+# if [[ $age -gt 18 && $age -lt 30 ]]
+# then
+# echo "valid age"
+# else
+# echo "invlid age"
+# fi
+# echo -e "Enter the file name: \c"
+# read file
+# if [ -f $file ]
+# then
+# if [ -w $file ]
+# then
+# echo "Type some text then press ctrl+d "
+# cat >> $file
+# else
+# echo "file do not have the write permission"
+# fi
+# else
+# echo "$file do not exist"
+# fi
+# echo -e "enter the file name: \c"
+# read file
+# if [ -f $file ]
+# then
+#     if [ -w $file ]
+#     then 
+#         echo "Type some text data. To quit press ctrl+d" 
+#         cat >> $file
+        
+#     else
+#         echo "The file do not have write permissions"  
+#         fi  
+# else
+# echo "$file not found"
+# fi
+
+
+# read -p "enter name:" name
+# read -sp "password:" pass
+# echo 
+# if [ $pass -eq 1234 ]
+# then
+# echo "WELCOME $name"
+# else
+# echo "wrong password"
+# fi
 
 # echo $1 $2 $3 "> echo $1 $2 $3"
 # args="$@"
